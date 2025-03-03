1161ec36321eaf8ff99598b114efe82bdf75f019
gabinschiro <gabin.schiro@epitech.eu>
2025-03-03 11:02:22 +0100

    license: add CeCILL-C license header to all source files
    Apply the CeCILL-C license header consistently across all source and test files in the project. This ensures:
    - Uniform licensing information
    - Clear copyright attribution
    - Standardized legal notice for the OpalLang project

--- src/lexer/Lexer.hpp
+++ src/lexer/Lexer.hpp
@@ -1,3 +1,24 @@
+/* OpalLang
+ * Copyright (C) 2025 OpalLang
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the CeCILL-C license as published by CEA, CNRS, and Inria,
+ * either version 1.0 of the License or (at your option) any later version.
+ *
+ * This software is distributed "as is," without any warranty of any kind,
+ * either express or implied, including but not limited to the warranties of
+ * merchantability or fitness for a particular purpose. See the CeCILL-C license
+ * for more details.
+ *
+ * You should have received a copy of the CeCILL-C license along with this
+ * program. If not, see https://cecill.info.
+ *
+ * Opal is a programming language designed with a focus on readability and
+ * performance. It combines modern programming concepts with a clean syntax,
+ * making it accessible to newcomers while providing the power and flexibility
+ * needed for experienced developers.
+*/
+
 #pragma once
 
 #include "Token.hpp"
